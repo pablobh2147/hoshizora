203d2f5c3780bbf76cb8a4b77b2a324350df5e8b
pablobh2147 <github@pablobh.com>
2026-06-07 03:40:40 +0200

    Simplify Camera class by removing ray precalculation and internal state management

--- inc/Camera.hpp
+++ inc/Camera.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <vector>
-
 #include "math/Matrix.hpp"
 #include "math/Vector.hpp"
 
@@ -11,57 +9,19 @@ class Camera {
    public:
     Camera(float fov, float near, float far);
 
-    void Resize(uint32_t width, uint32_t height);
-
     const Matrix4f& GetProjection() const { return m_projection; }
-    const Matrix4f& GetInverseProjection() const { return m_inverse_projection; }
     const Matrix4f& GetView() const { return m_view; }
-    const Matrix4f& GetInverseView() const { return m_inverse_view; }
 
-    const Vector3f& GetPosition() const { return m_position; }
-    const Vector3f& GetDirection() const { return m_direction; }
-
-    void SetPosition(const Vector3f& position) {
-        m_position = position;
-        CalculateView();
-        CalculateRays();
-    }
-
-    void SetDirection(const Vector3f& direction) {
-        m_direction = direction;
-        CalculateView();
-        CalculateRays();
-    }
-
-    void Move(const Vector3f& delta) {
-        m_position += delta;
-        CalculateView();
-        CalculateRays();
-    }
-
-    Vector3f GetRayDirection(uint32_t x, uint32_t y) const;
+    void CalculateProjection(float aspect);
+    void CalculateView(Vector3f position, Vector3f target, Vector3f up);
 
    private:
-    void CalculateProjection();
-    void CalculateView();
-    void CalculateRays();
-
     Matrix4f m_projection = {1};
-    Matrix4f m_inverse_projection = {1};
     Matrix4f m_view = {1};
-    Matrix4f m_inverse_view = {1};
 
-    float m_vertical_fov = 0.0F;
+    float m_fov = 0.0F;
     float m_near = 0.0F;
     float m_far = 0.0F;
-
-    Vector3f m_position = {0, 0, 0};
-    Vector3f m_direction = {0, 0, 0};
-
-    std::vector<Vector3f> m_rays;
-
-    uint32_t m_viewport_width = 0;
-    uint32_t m_viewport_height = 0;
 };
 
 }  // namespace hzr
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -4,44 +4,14 @@
 
 namespace hzr {
 
-Camera::Camera(float fov, float near, float far) : m_vertical_fov(fov), m_near(near), m_far(far) {}
+Camera::Camera(float fov, float near, float far) : m_fov(fov), m_near(near), m_far(far) {}
 
-void Camera::CalculateProjection() {
-    m_projection = glm::perspective(glm::radians(m_vertical_fov), float(m_viewport_width) / float(m_viewport_height), m_near, m_far);
-    m_inverse_projection = glm::inverse(m_projection);
+void Camera::CalculateProjection(float aspect) {
+    m_projection = glm::perspective(glm::radians(m_fov), aspect, m_near, m_far);
 }
 
-void Camera::CalculateView() {
-    m_view = glm::lookAt(m_position, m_position + m_direction, Vector3f(0, 1, 0));
-    m_inverse_view = glm::inverse(m_view);
-}
-
-void Camera::CalculateRays() {
-    m_rays.resize(m_viewport_width * m_viewport_height);
-
-    for (int y = 0; y < m_viewport_height; y++) {
-        for (int x = 0; x < m_viewport_width; x++) {
-            Vector2f coord = Vector2f(float(x) / float(m_viewport_width), float(y) / float(m_viewport_height));
-            coord = coord * 2.0f - 1.0f;
-
-            Vector4f ray = m_inverse_projection * Vector4f(coord.x, coord.y, 1.0f, 1.0f);
-            Vector3f ray_direction = Vector3f(m_inverse_view * Vector4f(glm::normalize(Vector3f(ray) / ray.w), 0));
-            m_rays[x + y * m_viewport_width] = ray_direction;
-        }
-    }
-}
-
-void Camera::Resize(uint32_t width, uint32_t height) {
-    m_viewport_width = width;
-    m_viewport_height = height;
-
-    CalculateProjection();
-    CalculateView();
-    CalculateRays();
-}
-
-Vector3f Camera::GetRayDirection(uint32_t x, uint32_t y) const {
-    return m_rays[x + y * m_viewport_width];
+void Camera::CalculateView(Vector3f position, Vector3f target, Vector3f up) {
+    m_view = glm::lookAt(position, target, up);
 }
 
 }  // namespace hzr
